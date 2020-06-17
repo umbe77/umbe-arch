f771a9cabc690288e18c38a1d515c0e8a6a45c73
umbe77 <umbe77@gmail.com>
2020-06-17 17:45:35 +0200

    fix yay install

--- 001-AUR.sh
+++ 001-AUR.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 aur_install() {
-    if pacman -Qi $1 &> /dev/null; then
+    if yay -Qi $1 &> /dev/null; then
         echo "Package "$1" just installed"
     else
         echo "Installing package "$1""
-        sudo pacman -S --noconfirm --needed $1
+        yay -S --noconfirm --needed $1
     fi
 }
 
