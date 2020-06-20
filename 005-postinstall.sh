b0a66e328c31bec03092caac3b2d9b5976a59b3e
umbe77 <umbe77@gmail.com>
2020-06-20 17:03:07 +0200

    typo NetworkService to NetworkManager

--- 005-postinstall.sh
+++ 005-postinstall.sh
@@ -4,7 +4,7 @@ set_current_city 1
 polybar_weather 1
 
 sudo systemctl enable sddm
-sudo systemctl enable NetworkService
+sudo systemctl enable NetworkManager
 
 nvm install 12
 sudo snap install code --classic
